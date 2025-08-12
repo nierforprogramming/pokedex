50025267e73168530869ff707e11393c563d3fc1
Nier <nierforprogramming@gmail.com>
2025-08-12 10:26:43 +0000

    logo icon added

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 
+import pokemonLogoIcon from "/pokemon-logo-icon.png";
+
 const Logo = ({ text }) => {
   return (
     <div className="logo">
+      <img src={pokemonLogoIcon} alt="Pokemon Logo Icon" />
       <h1>{text}</h1>
     </div>
   );
